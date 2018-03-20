68c8132def4db13b89e993883e571405b1914fdf
심성진-PC\심성진 <kumoh130715@gmail.com>
2018-03-20 23:34:02 +0900

    메인 페이지를 꾸미는 php파일
    main을 꾸미는 파일 추가

---
+++ mainprotoss.php
@@ -0,0 +1,19 @@
+<?php
+	require_once("session.php");
+	require_once("header.php");
+?>
+<header><h1>protoss</h1></header>
+<nav>
+<ul>
+	<li><a href="insertunitform.php">삽입</a></li>
+	<li><a href="deleteform.php">삭제</a></li>
+	<li><a href="reviseform.php">변경</a></li>
+	<li><a href="searchform.php">검색</a></li>
+</ul>
+</nav>
+<button id="btn">표시 안함</button>
+
+<section class="model">
+	<div id="write_review"><a href="writereviewform.php">리뷰 쓰기</a></div>
+	<div id="refresh">리뷰 보기</div>
+</section>
